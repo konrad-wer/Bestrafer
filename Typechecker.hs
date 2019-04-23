2c84f43d8dbe5d18108a1297b378e72103547dab
Konrad Werbliński <konrad.werblinski@gmail.com>
2019-04-23 18:08:58 +0200

    Refactored Error p constructors and code that used them and made applyContextToMonotype and applyContextToProposition monadic.

--- Typechecker.hs
+++ Typechecker.hs
@@ -5,15 +5,12 @@ import qualified Data.Set as Set
 
 data Error p
   = UndeclaredVariableError p Var
-  | InternalCompilerError String
+  | InternalCompilerError p String
   | ETypeVarMismatchError p Monotype Monotype
-  | UndeclaredETypeVarError p Var
-  | TypeFormednessEVarUndeclaredError p Var
-  | TypeFormednessUVarUndeclaredError p Var
-  | TypeFormednessInvalidKindError p Var
-  | CheckKindHasWrongKindError p Kind Kind
-  | CheckKindEVarUndeclaredError p Var
-  | CheckKindUVarUndeclaredError p Var
+  | UndeclaredETypeVarError p ETypeVar
+  | UndeclaredUTypeVarError p UTypeVar
+  | TypeHasWrongKindError p Type Kind Kind
+  | MonotypeHasWrongKindError p Monotype Kind Kind
   | MonotypeIsNotTypeError p Monotype
   | TypeFormednessPrcFEVError p [ETypeVar]
   deriving (Show, Eq)
@@ -61,16 +58,6 @@ uTypeVarEqContextLookup (CTypeVar (U b) _ : c) a
 uTypeVarEqContextLookup (_ : c) a = uTypeVarEqContextLookup c a
 uTypeVarEqContextLookup [] _ = Nothing
 
-solvedETypeVarContextLookup :: Context -> ETypeVar -> Maybe ContextEntry
-solvedETypeVarContextLookup (entry @ (CETypeVar b _ _) : c) a
-  | a == b = return entry
-  | otherwise = solvedETypeVarContextLookup c a
-solvedETypeVarContextLookup (CTypeVar (E b) _ : c) a
-  | a == b = Nothing
-  | otherwise = solvedETypeVarContextLookup c a
-solvedETypeVarContextLookup (_ : c) a = solvedETypeVarContextLookup c a
-solvedETypeVarContextLookup [] _ = Nothing
-
 eTypeVarContextLookup :: Context -> ETypeVar -> Maybe ContextEntry
 eTypeVarContextLookup (entry @ (CETypeVar b _ _) : c) a
   | a == b = return entry
@@ -97,7 +84,7 @@ eTypeVarContextReplace (entry @  (CTypeVar (E (ETypeVar b)) k) : ct) (ETypeVar a
   | a == b = return $ CETypeVar (ETypeVar a) k sigma : ct
   | otherwise = (:) entry <$> eTypeVarContextReplace ct (ETypeVar a) sigma p
 eTypeVarContextReplace (entry : ct) a sigma p = (:) entry <$> eTypeVarContextReplace ct a sigma p
-eTypeVarContextReplace [] (ETypeVar a) _ p = Left $ UndeclaredETypeVarError p a
+eTypeVarContextReplace [] a _ p = Left $ UndeclaredETypeVarError p a
 
 monotypeToType :: Monotype -> p -> Either (Error p) Type
 monotypeToType MUnit _ = return TUnit
@@ -111,40 +98,48 @@ monotypeToType n p = Left $ MonotypeIsNotTypeError p n
 applyContextToType :: Context -> Type -> p-> Either (Error p) Type
 applyContextToType c (TUVar u) p =
   case uTypeVarEqContextLookup c u of
-    Just (CUTypeVarEq _ tau) -> monotypeToType (applyContextToMonotype c tau) p
+    Just (CUTypeVarEq _ tau) -> applyContextToMonotype c tau p >>= flip monotypeToType p
     _ -> return $ TUVar u
-applyContextToType c (TImp pr t) p = TImp (applyContextToProposition c pr) <$> applyContextToType c t p
-applyContextToType c (TAnd t pr) p = TAnd <$> applyContextToType c t p <*> pure (applyContextToProposition c pr)
+applyContextToType c (TImp pr t) p = TImp <$> applyContextToProposition c pr p <*> applyContextToType c t p
+applyContextToType c (TAnd t pr) p = TAnd <$> applyContextToType c t p <*> applyContextToProposition c pr p
 applyContextToType c (TArrow t1 t2) p = TArrow <$> applyContextToType c t1 p <*> applyContextToType c t2 p
 applyContextToType c (TCoproduct t1 t2) p = TCoproduct <$> applyContextToType c t1 p <*> applyContextToType c t2 p
 applyContextToType c (TProduct t1 t2) p = TProduct <$> applyContextToType c t1 p <*> applyContextToType c t2 p
-applyContextToType c (TVec n t) p = TVec (applyContextToMonotype c n) <$> applyContextToType c t p
+applyContextToType c (TVec n t) p = TVec <$> applyContextToMonotype c n p <*> applyContextToType c t p
 applyContextToType c (TEVar e) p =
-  case solvedETypeVarContextLookup c e of
-    Just (CETypeVar _ _ tau) -> monotypeToType (applyContextToMonotype c tau) p
-    _ -> return $ TEVar e
+  case eTypeVarContextLookup c e of
+    Just (CETypeVar _ KStar tau) -> applyContextToMonotype c tau p >>= flip monotypeToType p
+    Just (CTypeVar _ KStar) -> return $ TEVar e
+    Just (CETypeVar _ KNat _) -> Left $ TypeHasWrongKindError p (TEVar e) KStar KNat
+    Just (CTypeVar _ KNat) -> Left $ TypeHasWrongKindError p (TEVar e) KStar KNat
+    Nothing -> Left $ UndeclaredETypeVarError p e
+    _ -> Left $ InternalCompilerError p "eTypeVarContextLookup"
 applyContextToType c (TUniversal a k t) p = TUniversal a k <$> applyContextToType (CTypeVar (U $ UTypeVar a) k : c) t p --TODO przemyśleć / zapytać
 applyContextToType c (TExistential a k t) p = TExistential a k <$> applyContextToType (CTypeVar (E $ ETypeVar a) k : c) t p
 applyContextToType _ TUnit _ = return TUnit
 
-applyContextToMonotype :: Context -> Monotype -> Monotype
-applyContextToMonotype c (MUVar u) =
+applyContextToMonotype :: Context -> Monotype -> p -> Either (Error p) Monotype
+applyContextToMonotype c (MUVar u) p =
   case uTypeVarEqContextLookup c u of
-    Just (CUTypeVarEq _ tau) -> applyContextToMonotype c tau
-    _ -> MUVar u
-applyContextToMonotype c (MArrow m1 m2) = MArrow (applyContextToMonotype c m1) (applyContextToMonotype c m2)
-applyContextToMonotype c (MCoproduct m1 m2) = MCoproduct (applyContextToMonotype c m1) (applyContextToMonotype c m2)
-applyContextToMonotype c (MProduct m1 m2) = MProduct (applyContextToMonotype c m1) (applyContextToMonotype c m2)
-applyContextToMonotype c (MEVar e) =
-  case solvedETypeVarContextLookup c e of
-    Just (CETypeVar _ _ tau) -> applyContextToMonotype c tau
-    _ -> MEVar e
-applyContextToMonotype _ MUnit = MUnit
-applyContextToMonotype _ MZero = MZero
-applyContextToMonotype c (MSucc n) = MSucc (applyContextToMonotype c n)
+    Just (CUTypeVarEq _ tau) -> applyContextToMonotype c tau p
+    _ -> return $ MUVar u
+applyContextToMonotype c (MArrow m1 m2) p = MArrow <$> applyContextToMonotype c m1 p <*> applyContextToMonotype c m2 p
+applyContextToMonotype c (MCoproduct m1 m2) p = MCoproduct <$> applyContextToMonotype c m1 p <*> applyContextToMonotype c m2 p
+applyContextToMonotype c (MProduct m1 m2) p = MProduct <$> applyContextToMonotype c m1 p <*> applyContextToMonotype c m2 p
+applyContextToMonotype c (MEVar e) p =
+  case eTypeVarContextLookup c e of
+    Just (CETypeVar _ _ tau) -> applyContextToMonotype c tau p
+    Just (CTypeVar _ _) -> return $ MEVar e
+    _ -> Left $ UndeclaredETypeVarError p e
+applyContextToMonotype _ MUnit _ = return MUnit
+applyContextToMonotype _ MZero _ = return MZero
+applyContextToMonotype c (MSucc n) p = MSucc <$> applyContextToMonotype c n p
 
-applyContextToProposition :: Context -> Proposition -> Proposition
-applyContextToProposition c (m1, m2) = (applyContextToMonotype c m1, applyContextToMonotype c m2)
+applyContextToProposition :: Context -> Proposition -> p -> Either (Error p) Proposition
+applyContextToProposition c (m1, m2) p = do
+  m1' <- applyContextToMonotype c m1 p
+  m2' <- applyContextToMonotype c m2 p
+  return (m1', m2')
 
 checkTypeWellFormednessWithPrnc :: Context -> Type -> Principality -> p -> Either (Error p) ()
 checkTypeWellFormednessWithPrnc c t NotPrincipal p = checkTypeWellFormedness c t p
@@ -163,17 +158,20 @@ checkTypeWellFormedness c (TAnd t pr) p = checkTypeWellFormedness c t p >> check
 checkTypeWellFormedness c (TUniversal x k t) p = checkTypeWellFormedness (CTypeVar (U $ UTypeVar x) k : c) t p
 checkTypeWellFormedness c (TExistential x k t) p = checkTypeWellFormedness (CTypeVar (E $ ETypeVar x) k : c) t p
 checkTypeWellFormedness c (TVec n t) p = checkMonotypeHasKind c n p KNat >> checkTypeWellFormedness c t p
-checkTypeWellFormedness c (TEVar x @ (ETypeVar name)) p =
+checkTypeWellFormedness c (TEVar x) p =
   case eTypeVarContextLookup c x of
     Just (CTypeVar _ KStar) -> return ()
     Just (CETypeVar _ KStar _) -> return ()
-    Nothing -> Left $ TypeFormednessEVarUndeclaredError p name
-    _ -> Left $ TypeFormednessInvalidKindError p name
-checkTypeWellFormedness c (TUVar x  @ (UTypeVar name)) p =
+    Just (CTypeVar _ KNat) -> Left $ TypeHasWrongKindError p (TEVar x) KStar KNat
+    Just (CETypeVar _ KNat _) -> Left $ TypeHasWrongKindError p (TEVar x) KStar KNat
+    Nothing -> Left $ UndeclaredETypeVarError p x
+    _ -> Left $ InternalCompilerError p "eTypeVarContextLookup"
+checkTypeWellFormedness c (TUVar x) p =
   case unsolvedTypeVarContextLookup c (U x) of
     Just (CTypeVar _ KStar) -> return ()
-    Nothing -> Left $ TypeFormednessUVarUndeclaredError p name
-    _ -> Left $ TypeFormednessInvalidKindError p name
+    Just (CTypeVar _ KNat) -> Left $ TypeHasWrongKindError p (TUVar x) KStar KNat
+    Nothing -> Left $ UndeclaredUTypeVarError p x
+    _ -> Left $ InternalCompilerError p "checkTypeWellFormedness"
 
 checkMonotypeHasKind :: Context -> Monotype -> p -> Kind -> Either (Error p) ()
 checkMonotypeHasKind c m p k1 = do
@@ -181,7 +179,7 @@ checkMonotypeHasKind c m p k1 = do
   if k1 == k2 then
     return ()
   else
-    Left $ CheckKindHasWrongKindError p k1 k2
+    Left $ MonotypeHasWrongKindError p m k1 k2
 
 inferMonotypeKind :: Context -> Monotype -> p -> Either (Error p) Kind
 inferMonotypeKind _ MUnit _ = return KStar
@@ -190,15 +188,15 @@ inferMonotypeKind c (MSucc n) p = checkMonotypeHasKind c n p KNat >> return KNat
 inferMonotypeKind c (MArrow m1 m2) p = checkMonotypeHasKind c m1 p KStar >> checkMonotypeHasKind c m2 p KStar >> return KStar
 inferMonotypeKind c (MCoproduct m1 m2) p = checkMonotypeHasKind c m1 p KStar >> checkMonotypeHasKind c m2 p KStar >> return KStar
 inferMonotypeKind c (MProduct m1 m2) p = checkMonotypeHasKind c m1 p KStar >> checkMonotypeHasKind c m2 p KStar >> return KStar
-inferMonotypeKind c (MEVar x @ (ETypeVar name)) p =
+inferMonotypeKind c (MEVar x) p =
   case eTypeVarContextLookup c x of
     Just (CTypeVar _ k) -> return k
     Just (CETypeVar _ k _) -> return k
-    _ ->  Left $ CheckKindEVarUndeclaredError p name
-inferMonotypeKind c (MUVar x @ (UTypeVar name)) p =
+    _ ->  Left $ UndeclaredETypeVarError p x
+inferMonotypeKind c (MUVar x) p =
   case unsolvedTypeVarContextLookup c (U x) of
     Just (CTypeVar _ k) -> return k
-    _ -> Left $ CheckKindUVarUndeclaredError p name
+    _ -> Left $ UndeclaredUTypeVarError p x
 
 checkPropWellFormedness :: Context -> Proposition -> p -> Either (Error p) ()
 checkPropWellFormedness c (m1, m2) p = inferMonotypeKind c m1 p >>= checkMonotypeHasKind c m2 p
